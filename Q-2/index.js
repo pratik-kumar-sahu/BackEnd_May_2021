4c816421c6b752facd1e533aca4bae1a6ae85f16
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-05-31 09:19:06 +0530

    heroku commit

--- Q-2/index.js
+++ Q-2/index.js
@@ -1,14 +1,17 @@
 function getAge(DOB) {
-  var today = new Date();
-  var birthDate = new Date(DOB);
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  var birthMonth = birthDate.getMonth();
-  var birthDay = birthDate.getDay();
+  const today = new Date();
+  const birthDate = new Date(DOB);
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds()
+  const m = today.getMonth() - birthDate.getMonth();
+  const birthMonth = birthDate.getMonth();
+  const birthDay = birthDate.getDay();
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     age--;
   }
-  return `${age} years ${birthMonth + 1} months ${birthDay + 1} days`;
+  return `${age} years ${birthMonth + 1} months ${birthDay + 1} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 }
 
-console.log(getAge("2002/05/30"));
+console.log(getAge("2002/05/31"));
